ed22d8e67528f2461c54de0a3641c02cace8e5c7
Nathan Bertrac <nbertrac@sollan.com>
2024-03-13 11:19:15 +0100

    Merge branch 'main' of github.com:faresmez/Projet-annuel

--- back-end/controllers/userDrawingsController.js
+++ back-end/controllers/userDrawingsController.js
@@ -19,25 +19,38 @@ exports.getAllNumbers = async (req, res) => {
     const numbers = await Numbers.find();
     res.json(numbers);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: "Erreur lors de la récupération des dessins utilisateurs",
-      });
+    res.status(500).json({
+      error: "Erreur lors de la récupération des dessins utilisateurs",
+    });
   }
 };
 
 // detail
 exports.getNumberById = async (req, res) => {
   try {
-    const Number = await Numbers.findById(req.params.id);
-    if (!Number) {
+    const numberToGet = await Numbers.findById(req.params.id);
+    if (!numberToGet) {
       return res.status(404).json({ error: "Number non trouvée" });
     }
-    res.json(Number);
+    res.json(numberToGet);
   } catch (error) {
     res
       .status(500)
       .json({ error: "Erreur lors de la récupération du dessin utilisateur" });
+  }
+};
+
+exports.deleteNumber = async (req, res) => {
+  try {
+    const numberToDelete = await Numbers.findByIdAndRemove(req.params.id);
+    if (!numberToDelete) {
+      return res.status(404).json({ error: "Dessin utilisateur non trouvée." });
+    }
+    res.json({ message: "Dessin utilisateur supprimée avec succès." });
+  } catch (err) {
+    res.status(500).json({
+      error:
+        "Une erreur s'est produite lors de la suppression du dessin utilisateur.",
+    });
   }
 };
